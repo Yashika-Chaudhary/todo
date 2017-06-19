1df7122e5d97f8c6aa62e0d4d074b21a30400d8e
yashika <yashikac17@gmail.com>
2017-06-19 12:43:19 +0530

    tenth commit

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ViewController, ModalController, AlertController } from 'ionic-angular';
-import * as moment from 'moment';
+
 
 
 @IonicPage()
@@ -13,14 +13,13 @@ export class AddPage
  {
 Description;
 Date;
-    eventSource = [];
-  viewTitle: string;
-  selectedDay = new Date();
-
-  calendar = {
-    mode: 'month',
-    currentDate: new Date()
-  };
+eventSource;
+    viewTitle;
+    isToday: boolean;
+    calendar = {
+        mode: 'month',
+        currentDate: new Date()
+    };
 
   constructor(public navCtrl: NavController,public alertCtrl: AlertController, public navParams: NavParams, public view: ViewController, public modalCtrl: ModalController){
 
@@ -38,45 +37,72 @@ Date;
        close(){
    this.view.dismiss();
  }
-
- addEvent() {
-    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
-    modal.present();
-    modal.onDidDismiss(data => {
-      if (data) {
-        let eventData = data;
-
-        eventData.startTime = new Date(data.startTime);
-        eventData.endTime = new Date(data.endTime);
-
-        let events = this.eventSource;
-        events.push(eventData);
-        this.eventSource = [];
-        setTimeout(() => {
-          this.eventSource = events;
-        });
-      }
-    });
-  }
-
-  onViewTitleChanged(title) {
-    this.viewTitle = title;
-  }
-
-  onEventSelected(event) {
-    let start = moment(event.startTime).format('LLLL');
-    let end = moment(event.endTime).format('LLLL');
-
-    let alert = this.alertCtrl.create({
-      title: '' + event.title,
-      subTitle: 'From: ' + start + '<br>To: ' + end,
-      buttons: ['OK']
-    })
-    alert.present();
-  }
-
-  onTimeSelected(ev) {
-    this.selectedDay = ev.selectedTime;
-  }
+ loadEvents() {
+         this.eventSource = this.createRandomEvents();
+     }
+     onViewTitleChanged(title) {
+         this.viewTitle = title;
+     }
+     onEventSelected(event) {
+         console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+     }
+    
+     today() {
+         this.calendar.currentDate = new Date();
+     }
+     onTimeSelected(ev) {
+         console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+             (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+     }
+     onCurrentDateChanged(event:Date) {
+         var today = new Date();
+         today.setHours(0, 0, 0, 0);
+         event.setHours(0, 0, 0, 0);
+         this.isToday = today.getTime() === event.getTime();
+     }
+     createRandomEvents() {
+         var events = [];
+         for (var i = 0; i < 50; i += 1) {
+             var date = new Date();
+             var eventType = Math.floor(Math.random() * 2);
+             var startDay = Math.floor(Math.random() * 90) - 45;
+             var endDay = Math.floor(Math.random() * 2) + startDay;
+             var startTime;
+             var endTime;
+             if (eventType === 0) {
+                 startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+                 if (endDay === startDay) {
+                     endDay += 1;
+                 }
+                 endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+                 events.push({
+                     title: 'All Day - ' + i,
+                     startTime: startTime,
+                     endTime: endTime,
+                     allDay: true
+                 });
+             } else {
+                 var startMinute = Math.floor(Math.random() * 24 * 60);
+                 var endMinute = Math.floor(Math.random() * 180) + startMinute;
+                 startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+                 endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+                 events.push({
+                     title: 'Event - ' + i,
+                     startTime: startTime,
+                     endTime: endTime,
+                     allDay: false
+                 });
+             }
+         }
+         return events;
+     }
+     onRangeChanged(ev) {
+         console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+     }
+     markDisabled = (date:Date) => {
+         var current = new Date();
+         current.setHours(0, 0, 0);
+         return date < current;
+     };
 
  }
